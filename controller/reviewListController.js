dce519057faaa6d076132229baa2ba8656925086
sainohira <xsainohira.shota@contract.isid.co.jp>
2018-07-18 05:18:07 +0000

    EC2上での動作確認

--- controller/reviewListController.js
+++ controller/reviewListController.js
@@ -37,7 +37,7 @@ MAIN.startController = function startController(cConnectionString, cController, 
         });
     });
 
-    controller.hears(['rs ', 'review status'], 'direct_message,direct_mention,mention', (bot, message) => {
+    controller.hears(['rs', 'review status', 'レビューステータス'], 'direct_message,direct_mention,mention', (bot, message) => {
         let jsonList = {
             "fallback": "ボタン操作のできない端末かブラウザです。",
             "callback_id": "reviewList",
